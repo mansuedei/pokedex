54c2e03821ce4099dbc3dd8445fb0e161fc88c9f
Iana Stepanova <Iana_Stepanova@epam.com>
2020-03-26 21:49:02 +0300

    cleared previously layed component structure in order to rework

--- src/app/Pokemon/Pokemon.module.ts
+++ src/app/Pokemon/Pokemon.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from '../components/home/home.component';
+import { HomeComponent } from './components/home/home.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
-import { PokemonCardComponent } from '../components/pokemon-card/pokemon-card.component';
+import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.component';
 
 
 
