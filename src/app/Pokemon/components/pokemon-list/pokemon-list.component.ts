8dcd1c76147e056328f590584370019c0d9fa214
Iana Stepanova <Iana_Stepanova@epam.com>
2020-03-27 00:34:05 +0300

    added the damage property to the ArrayOfPokemons

--- src/app/Pokemon/components/pokemon-list/pokemon-list.component.ts
+++ src/app/Pokemon/components/pokemon-list/pokemon-list.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
+
+import { pokemonWithDamage } from "../../pokemon";
 
 @Component({
-  selector: 'app-pokemon-list',
-  templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.scss']
+  selector: "app-pokemon-list",
+  templateUrl: "./pokemon-list.component.html",
+  styleUrls: ["./pokemon-list.component.scss"]
 })
-export class PokemonListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class PokemonListComponent {
+  pokemon = pokemonWithDamage;
 }
