f403f8efda9f51d301d22048348218ec8b230283
Iana Stepanova <Iana_Stepanova@epam.com>
2020-03-27 16:02:58 +0300

    unlinked and deleted header component

--- src/app/Pokemon/Pokemon.module.ts
+++ src/app/Pokemon/Pokemon.module.ts
@@ -3,19 +3,17 @@ import { CommonModule } from "@angular/common";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 
-import { HeaderComponent } from "./components/header/header.component";
 import { PokemonListComponent } from "./components/pokemon-list/pokemon-list.component";
 import { PokemonItemComponent } from "./components/pokemon-item/pokemon-item.component";
 import { PokemonCardComponent } from "./components/pokemon-card/pokemon-card.component";
 
 @NgModule({
   declarations: [
-    HeaderComponent,
     PokemonListComponent,
     PokemonItemComponent,
     PokemonCardComponent
   ],
   imports: [CommonModule, BrowserModule, FormsModule],
-  exports: [HeaderComponent, PokemonListComponent]
+  exports: [PokemonListComponent]
 })
 export class PokemonModule {}
--- src/app/Pokemon/components/header/header.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from "@angular/core";
-import { ThrowStmt } from "@angular/compiler";
-
-@Component({
-  selector: "app-header",
-  templateUrl: "./header.component.html",
-  styleUrls: ["./header.component.scss"]
-})
-export class HeaderComponent {}
