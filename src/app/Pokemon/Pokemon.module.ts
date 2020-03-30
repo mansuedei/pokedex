55a37024417f37ea02362dd5f7eaf2d3b1ed3af8
Iana Stepanova <Iana_Stepanova@epam.com>
2020-03-30 16:43:35 +0300

    implemented border highlighting via custom directive

--- src/app/Pokemon/Pokemon.module.ts
+++ src/app/Pokemon/Pokemon.module.ts
@@ -6,12 +6,14 @@ import { FormsModule } from "@angular/forms";
 import { PokemonListComponent } from "./components/pokemon-list/pokemon-list.component";
 import { PokemonItemComponent } from "./components/pokemon-item/pokemon-item.component";
 import { PokemonCardComponent } from "./components/pokemon-card/pokemon-card.component";
+import { HighlightDamageDirective } from "./directives/highlight-damage.directive";
 
 @NgModule({
   declarations: [
     PokemonListComponent,
     PokemonItemComponent,
-    PokemonCardComponent
+    PokemonCardComponent,
+    HighlightDamageDirective
   ],
   imports: [CommonModule, BrowserModule, FormsModule],
   exports: [PokemonListComponent]
---
+++ src/app/Pokemon/directives/highlight-damage.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HighlightDamageDirective } from './highlight-damage.directive';
+
+describe('HighlightDamageDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HighlightDamageDirective();
+    expect(directive).toBeTruthy();
+  });
+});
