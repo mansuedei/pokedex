2e372773971a7108d0c6aae7fe939bfe0327d325
Iana Stepanova <Iana_Stepanova@epam.com>
2020-04-02 19:54:56 +0300

    implemented pokemon-details component with OnPush DetectionStrategy

--- src/app/Pokemon/components/pokemon-details/pokemon-details.component.ts
+++ src/app/Pokemon/components/pokemon-details/pokemon-details.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+
+import { Pokemons } from '../../pokemon';
+import { PokemonClass } from '../../../models/PokemonClass';
 
 @Component({
   selector: 'app-pokemon-details',
   templateUrl: './pokemon-details.component.html',
-  styleUrls: ['./pokemon-details.component.scss']
+  styleUrls: ['./pokemon-details.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PokemonDetailsComponent implements OnInit {
+export class PokemonDetailsComponent {
+  @Input() pokemons = Pokemons;
+  caught = false;
 
-  constructor() { }
+  @Output() onStatusChange = new EventEmitter<string>();
 
-  ngOnInit(): void {
+  changePokemonStatus(e): void {
+    this.caught = e.target.checked;
+    if (!this.caught) {
+      this.onStatusChange.emit(
+        `${this.pokemons[0].id} was successfully released!`
+      );
+    } else {
+      this.onStatusChange.emit(
+        `${this.pokemons[0].id} was successfully caught!`
+      );
+    }
   }
-
 }
