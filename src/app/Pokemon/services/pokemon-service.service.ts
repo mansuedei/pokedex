ec79375c8f5bb3186cee88acdb6305269985941b
Iana Stepanova <Iana_Stepanova@epam.com>
2020-04-06 20:26:16 +0300

    implemented getAll, getByName, getByID methods in the PokemonService

--- src/app/Pokemon/services/pokemon-service.service.ts
+++ src/app/Pokemon/services/pokemon-service.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Pokemons } from '../pokemon';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PokemonService {
+  pokemons = Pokemons;
 
-  constructor() { }
+  public getAll() {
+    return this.pokemons;
+  }
+
+  public getByName() {
+    return this.pokemons.filter(function (pokemon) {
+      return pokemon.name == 'bulbasaur'; // will be later changed to the search input value
+    });
+  }
+
+  public getByID() {
+    return this.pokemons.filter(function (pokemon) {
+      return pokemon.id == 2; // will be later changed to the search input value
+    });
+  }
 }
